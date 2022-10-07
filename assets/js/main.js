578bb2c26d8be2c952f50da13a4b654d9528d586
NamlidN <dilman.n999@gmail.com>
2022-10-07 16:34:47 +0200

    NEWSLETTERcheKEd

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,11 @@ function myFunction() {
     confirm('Newsletter Abbonieren?');
 }
 myFunction()
+function validate() {
+    if (document.getElementById('LetterNeed').checked) {
+        alert("checked");
+    } else {
+        alert("You didn't check it! Let me check it for you.");
+    }
+}    
+validate()
